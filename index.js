be15e88ed2df8cf4466e7c40d250721f60680b15
ashishb012 <ashishb0128@gmail.com>
2023-06-28 09:52:42 +0530

    fetch request

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ document.getElementById("send-btn").addEventListener("click", () => {
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`;
   }
 });
+
+const api = api_key;
+const url = "https://api.openai.com/v1/completions";
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${api}`,
+  },
+  body: JSON.stringify({
+    model: "text-davinci-003",
+    prompt: "",
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => (movieBossText.innerText = data.choices[0].text));
